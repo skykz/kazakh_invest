49a8c35be114a1b42ae3868e023abecb34f7e006
Yerassyl <erasylgeso@gmail.com>
2020-10-29 02:13:25 +0600

    init commit

---
+++ android/app/src/main/kotlin/com/example/kazakh_invest/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.kazakh_invest
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+import 'package:kazakh_invest/src/app.dart';
+
+void main() {
+  runApp(MyApp());
+}
